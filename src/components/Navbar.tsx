e8a7a080196e68280bfe8048392c08307f148327
Anubhav Bhambri <anubhavbhambri1@gmail.com>
2023-07-23 02:28:56 +0530

    created convince route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
-import { getAuthSession } from "@/lib/auth";
 import { UserAccountNav } from "./UserAccountNav";
+import SearchBar from "./SearchBar";
 
 const Navbar = async () => {
-  const session = await getAuthSession();
+  const session = await getServerSession(authOptions);
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7x1 h-full mx-auto flex items-center justify-between gap-1">
@@ -15,6 +17,7 @@ const Navbar = async () => {
             Social
           </p>
         </Link>
+        <SearchBar />
         {session?.user ? (
           <UserAccountNav user={session?.user} />
         ) : (
