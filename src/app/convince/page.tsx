7bd3c083c7e83893513a65ca5066a588c07bbb2b
Anubhav Bhambri <anubhavbhambri1@gmail.com>
2023-07-23 02:43:52 +0530

    added one convince event

--- src/app/convince/page.tsx
+++ src/app/convince/page.tsx
@@ -9,11 +9,53 @@ import { useMutation } from "@tanstack/react-query";
 import axios, { AxiosError } from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/Card";
+
+const convinces = [
+  {
+    title: "salary negotiation",
+    description:
+      "An HR manager in a tech startup is hiring a developer. Technical rounds are cleared. Now they have reached the negotiation point. Expectations of both parties is not matching.",
+    agentsCount: 2,
+    agent1: {
+      title: "HR Manager",
+    },
+    agent2: {
+      title: "Web Developer",
+    },
+  },
+];
 
 const Page = () => {
   return (
-    <div className="container flex items-center h-full max-w-3xl mx-auto">
-      Convince
+    <div className="container flex items-center flex-col h-full max-w-3xl mx-auto">
+      <p>Convince</p>
+      <div>
+        {convinces.map((conv, idx) => (
+          <Card key={idx}>
+            <CardHeader>
+              <CardTitle>{conv.title}</CardTitle>
+              <CardDescription>{conv.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <div>
+                <Button className="mr-5">Play {conv.agent1.title}</Button>
+                <Button>Play {conv.agent2.title}</Button>
+              </div>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
